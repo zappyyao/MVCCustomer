4f8763016e4604c956072427b9b6943f7137a7aa
zappyyao <ziv@shinda.com.tw>
2015-04-23 00:20:42 +0800

    專案建立完成

---
+++ MVCCustomer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCCustomer.Startup))]
+namespace MVCCustomer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
