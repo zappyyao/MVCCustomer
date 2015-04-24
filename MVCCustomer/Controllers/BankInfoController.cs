5129ddb6d5af5785ec72574f9b1e950f7d185708
zappyyao <ziv@shinda.com.tw>
2015-04-25 00:09:11 +0800

    作業初版

---
+++ MVCCustomer/Controllers/BankInfoController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCCustomer.Models;
+
+namespace MVCCustomer.Controllers
+{
+    public class BankInfoController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: BankInfo
+        public ActionResult Index()
+        {
+            var 客戶銀行資訊 = db.客戶銀行資訊.Include(客 => 客.客戶資料);
+            return View(客戶銀行資訊.ToList());
+        }
+
+        // GET: BankInfo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶銀行資訊);
+        }
+
+        // GET: BankInfo/Create
+        public ActionResult Create()
+        {
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱");
+            return View();
+        }
+
+        // POST: BankInfo/Create
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,客戶Id,銀行名稱,銀行代碼,分行代碼,帳戶名稱,帳戶號碼")] 客戶銀行資訊 客戶銀行資訊)
+        {
+            if (ModelState.IsValid)
+            {
+                db.客戶銀行資訊.Add(客戶銀行資訊);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // GET: BankInfo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // POST: BankInfo/Edit/5
+        // 若要免於過量張貼攻擊，請啟用想要繫結的特定屬性，如需
+        // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,客戶Id,銀行名稱,銀行代碼,分行代碼,帳戶名稱,帳戶號碼")] 客戶銀行資訊 客戶銀行資訊)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(客戶銀行資訊).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.客戶Id = new SelectList(db.客戶資料, "Id", "客戶名稱", 客戶銀行資訊.客戶Id);
+            return View(客戶銀行資訊);
+        }
+
+        // GET: BankInfo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            if (客戶銀行資訊 == null)
+            {
+                return HttpNotFound();
+            }
+            return View(客戶銀行資訊);
+        }
+
+        // POST: BankInfo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            客戶銀行資訊 客戶銀行資訊 = db.客戶銀行資訊.Find(id);
+            db.客戶銀行資訊.Remove(客戶銀行資訊);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ MVCCustomer/Controllers/ReportController.cs
@@ -0,0 +1,27 @@
+﻿using MVCCustomer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCustomer.Controllers
+{
+    public class ReportController : Controller
+    {
+        private 客戶資料Entities db = new 客戶資料Entities();
+
+        // GET: Report
+        public ActionResult Index()
+        {
+            var data = db.客戶資料;
+            foreach (var item in data)
+            {
+                //item.客戶聯絡人.Count();
+            }
+
+
+            return View(data.ToList());
+        }
+    }
+}
